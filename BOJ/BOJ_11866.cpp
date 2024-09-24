3357e45bf21309cbbaee6e6ba18b5e52556f3413
Anon <codeparadoxist@gmail.com>
2024-09-24 23:25:11 +0900

    요세푸스 문제 0

--- BOJ/BOJ_11866.cpp
+++ BOJ/BOJ_11866.cpp
@@ -4,59 +4,49 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	// K<=n k번째 사람을 제거 큐
-	// 1,2,3,4,5,6,7
-	// q.pop() q.pop() q.pop() q.pop()
-	// 4,5,6,7
-	// pop() 
-	// push()
-	//3
+
 
 	int n, k;
 	queue <int> q;
-	queue <int> q2;
+
 	cin >> n >> k;
 
-	int num;
+
 	for (int i = 1; i <= n; i++) {
 
 
-			q.push(i);
-			
+		q.push(i);
+
 
 	}
 
 
 	int i = 1;
 	cout << "<";
-	while (q.empty()!=1) {
-		
-	
-		if (i %k!=0) {
-		
-			
+	while (q.empty() != 1) {
+
+
+		if (i < k) {
+
+
 			q.push(q.front());
 			q.pop();
 			i++;
-			
+
 		}
 
-		else if (i==3) {
-			
+		else if (i == k) {
+
 			cout << q.front();
-		
+
 			q.pop();
-		
-			if (q.empty() != 1)
+			if (q.empty() != 1) {
 				cout << ", ";
+			}
 			i = 1;
-
-
 		}
-		
-		
+
+
 	}
 
 	cout << ">";
