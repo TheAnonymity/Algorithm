667847f6e337cc630b53649e545502f52377ca3f
Anon <codeparadoxist@gmail.com>
2024-10-04 19:46:42 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ BOJ/BOJ_24262.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int MenOfPassion(int A[],int n) {
+    int i;
+    i = n / 2;
+    return A[i];
+}
+
+int main() {
+
+    int n;
+
+    cin >> n;
+
+    cout << 1 << '\n';
+    cout << 0;
+}
+
